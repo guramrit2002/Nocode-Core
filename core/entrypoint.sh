50df7c451c69b5c900d9664e619a67dda53041df
Guramrit Pal Singh <77382202+guramrit2002@users.noreply.github.com>
2025-03-30 22:27:02 +0530

    Create entrypoint.sh

---
+++ core/entrypoint.sh
@@ -0,0 +1,11 @@
+echo "⏳ Waiting for MySQL at $DB_HOST:$DB_PORT..."
+until nc -z $DB_HOST $DB_PORT; do
+  sleep 1
+done
+echo "✅ MySQL is up!"
+
+echo "🚀 Running migrations..."
+python manage.py migrate account admin auth contenttypes sessions socialaccount
+
+echo "🟢 Starting Django..."
+python manage.py runserver 0.0.0.0:8000
