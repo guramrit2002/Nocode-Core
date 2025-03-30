e561f4dc04947ad845d301c5e7caf4fce28cc0ce
guramrit <guramrit1066@gmail.com>
2025-03-31 02:16:43 +0530

    password change

--- core/user/handlers.py
+++ core/user/handlers.py
@@ -1,11 +1,30 @@
-from .models import UserProfile
 from rest_framework import status
-
+from .utils import UserUtils
+from .serializers import UserSerializer
 
 class UserHandler:
     
     @classmethod
-    def get_profile_by_id(cls,user_id):
-        user = UserProfile.objects.filter(id = user_id).values()
-        return user, status.HTTP_200_OK
+    def get_profile_handler(cls,user_id):
+        try:
+            user, profile_status = UserUtils.get_user_by_id(user_id=user_id)
+            
+            if profile_status == status.HTTP_200_OK:
+                return user.values(), status.HTTP_200_OK
+            return
+        except Exception as e:
+            return str(e), status.HTTP_400_BAD_REQUEST
     
+    @classmethod
+    def forget_password_handler(cls,user_id,new_password):
+        try:
+            user, profile_status = UserUtils.get_user_by_id(user_id=user_id)
+            if profile_status == status.HTTP_200_OK:
+                
+                password_change = UserUtils.change_password_by_id\
+                    (user=user.first(),new_password=new_password)
+                    
+                return ({"message":"Password is changed.","user":user.values()},
+                        status.HTTP_200_OK)
+        except Exception as e:
+            return {"errors":str(e)}, status.HTTP_400_BAD_REQUEST
--- core/user/views.py
+++ core/user/views.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 from .handlers import UserHandler
 
 class UserViewSet(ViewSet):
     
-    permission_classes = [IsAuthenticated]
-    
+    @permission_classes([IsAuthenticated])
     def get_profile(self,request,id):
-        data,status = UserHandler.get_profile_by_id(id)
-        return Response(data,status=status)
+        data,status = UserHandler.get_profile_handler(user_id=id)
+        return Response(data, status=status)
+    
+    def forget_password(self,request,id):
+        data,status = UserHandler.forget_password_handler(\
+            user_id=id,new_password=request.data.get("new_password"))
+        print(status)
+        return Response(data, status=status)
