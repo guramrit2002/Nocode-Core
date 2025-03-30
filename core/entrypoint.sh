a58c3679266304ca67ba09cc113ec94147a94270
Guramrit Pal Singh <77382202+guramrit2002@users.noreply.github.com>
2025-03-30 22:46:24 +0530

    Update entrypoint.sh

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -8,7 +8,7 @@ python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
     print("✅ Superuser created.")
 else:
     print("ℹ️ Superuser already exists.")
